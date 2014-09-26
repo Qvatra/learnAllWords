d175497fed18b3c961da96d6ea46ecb7c66217e1
unknown <Oleksandr@PC270.macaw.nl>
2014-09-26 22:41:06 +0200

    first commit

---
+++ mainController.js
@@ -0,0 +1,28 @@
+﻿angular.module('app', [])
+  .controller('mainController', ['$scope', function ($scope) {
+      $scope.mode = 'home';
+      $scope.word = 'word';
+      $scope.translate = 'слово';
+
+
+      $scope.home = function () {
+          console.log('home');
+          $scope.mode = 'home';
+      };
+
+      $scope.card = function () {
+          console.log('card');
+          $scope.mode = 'card';
+      };
+
+      $scope.rate = function () {
+          console.log('rate');
+          $scope.mode = 'rate';
+      };
+
+      $scope.settings = function () {
+          console.log('settings');
+          $scope.mode = 'settings';
+      };
+
+  }]);
